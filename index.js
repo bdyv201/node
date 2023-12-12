cacc5c8d57d6e44ec3947e4ae3f4c99dd9d1b431
Rodion Drennov <bdyv200@gmail.com>
2023-12-13 00:17:59 +0300

    new

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let express = require('express');
 let app = express();
 app.listen(3000);
 app.get("/", function(request, response){
-    // отправляем ответ
+
    response.send("<h2>Привет Express!</h2>");
 });
