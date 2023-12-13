4ccf8e8d45e6a5e8665feee5d73a2069222ae57a
Rodion Drennov <bdyv200@gmail.com>
2023-12-13 03:13:28 +0300

    done

--- routes/admin.js
+++ routes/admin.js
@@ -4,9 +4,9 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 const jsonParser = express.json();
-<<<<<<< HEAD
+//<<<<<<< HEAD
 //комментарий из ветки моделей
-=======
+//=======
 //Контроллеры для админ панели 
 >>>>>>> 3dfaa9dc9bee935b5b462c9ded3a7ce415b0dcb9
 router.get('/', adminController.getArticleAll);
